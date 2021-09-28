0a31e51d322ce3d7f7c7c836ceb5f6bf40493c33
Estefania <estefacdn@gmail.com>
2021-09-27 19:33:46 -0500

    creacion formulario

--- index.php
+++ index.php
@@ -8,7 +8,47 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 <body>
+    <header>
 
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-5">
+                    <form action="" method="">
+
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Nombres</label>
+                            <input type="email" class="form-control" name="">
+                        </div>    
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Apellidos</label>
+                            <input type="email" class="form-control" name="">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email</label>
+                            <input type="email" class="form-control" name="">
+                        </div>   
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Edad</label>
+                            <input type="email" class="form-control" name="">
+                        </div>  
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">descripcion</label>
+                            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" name=""></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">fotografia</label>
+                            <input type="email" class="form-control" name="">
+                        </div>
+
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+    </main>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>  
